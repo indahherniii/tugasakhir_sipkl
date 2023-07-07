82ef4f2b300999f84ba1ebfc2ccfc7d9cc544304
indahherniii <indahputriherni@gmail.com>
2023-07-07 16:32:57 +0700

    memperbaiki isi tampilan admin(master-data) pada menu pegawai dan menuh tambah pegawai

--- resources/views/admin/master-data/pegawai/create.blade.php
+++ resources/views/admin/master-data/pegawai/create.blade.php
@@ -1,61 +1,55 @@
 @extends('layouts.admin.main')
 
 @section('container')
-  <div class="page-content">
-    <div class="page-header">
-      <h4 class="page-title">Tambah Data Pegawai</h4>
-    </div>
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="card">
-          <div class="card-header">
-            <h5 class="card-title">Form Data Pegawai</h5>
-          </div>
-          <div class="card-body">
-            <form>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="nama">Nama</label>
-                  <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama">
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="jabatan">Jabatan</label>
-                  <input type="text" class="form-control" id="jabatan" placeholder="Masukkan Jabatan">
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="status">Status</label>
-                  <select class="form-control" id="unit_kerja">
-                    <option value="admin">admin</option>
-                    <option value="master-data">master-data</option>
-                  </select>
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="unit_kerja">Unit Kerja</label>
-                  <select class="form-control" id="unit_kerja">
-                    <option value="IT"> Tekonologi Informasi</option>
-                    <option value="Keuangan">Teknik Mesin</option>
-                    <option value="SDM">Agroindustri</option>
-                  </select>
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="email">Email</label>
-                  <input type="text" class="form-control" id="email" placeholder="email">
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="card-footer d-flex justify-content-end">
-            <a href="{{ url('/admin/master-data/pegawai/index') }}" class="btn btn-sm btn-primary">
-              <span class="btn-label">
-                <i data-feather="plus"></i>
-              </span>
-              Tambah Pegawai
-            </a>
-          </div>
+  <nav class="page-breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active" aria-current="page">Pegawai</li>
+    </ol>
+  </nav>
+
+  <div class="row">
+    <div class="col-md-12 grid-margin stretch-card">
+      <div class="card">
+        <div class="card-body">
+          <h6 class="card-title">Form Tambah Data Pegawai</h6>
+          <form>
+
+            <div class="mb-3">
+              <label for="Inputname" class="form-label">Nama</label>
+              <input type="text" class="form-control" id="name" value="Eka Wahyudi" placeholder="Enter Name">
+            </div>
+
+            <div class="mb-3">
+              <label for="FormControlStatus" class="form-label">Status</label>
+              <select class="form-select" id="FormControlStatus">
+                <option selected disabled>Select your status</option>
+                <option>Master-data</option>
+                <option>Admin</option>
+              </select>
+            </div>
+
+            <div class="mb-3">
+              <label for="InputEmail" class="form-label">Email Input</label>
+              <input type="email" class="form-control" id="InputEmail" value="ekawahyudi@gmail.com"
+                placeholder="Enter Email">
+            </div>
+
+            <div class="mb-3">
+              <label for="FormControlJabatan" class="form-label">Jabatan</label>
+              <select class="form-select" id="FormControlJabatan">
+                <option selected disabled>Select your jabatan</option>
+                <option>Direktur</option>
+                <option>Kepala Prodi</option>
+                <option>Kepala Jurusan</option>
+              </select>
+            </div>
+            <div class="d-flex justify-content-end">
+              <a href="{{ url('/admin/master-data/pegawai') }}" class="btn btn-primary">
+                <i data-feather="save" class="me-2"></i>
+                Simpan Data
+              </a>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- resources/views/admin/master-data/pegawai/index.blade.php
+++ resources/views/admin/master-data/pegawai/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.admin.main')
 
 @section('container')
-  <nav class="page-inner">
-    <div class="page-header">
-      <h4 class="page-title">Pegawai</h4>
-  </nav>
+  <div class="d-flex justify-content-center align-items-center flex-wrap grid-margin">
+    <div>
+      <h4 class="mb-3 mb-md-0">Pegawai</h4>
+    </div>
+  </div>
   <div class="row">
     <div class="col-md-12 grid-margin stretch-card">
       <div class="card">
